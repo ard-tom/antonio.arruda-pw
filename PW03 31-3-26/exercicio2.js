26555bfa2ad1f480fcb11d301a6798a4a2cb7eb6
Tom <tom.arruda009@gmail.com>
2026-03-31 16:14:20 -0300

    segundo commit

---
+++ PW03 31-3-26/exercicio2.js
@@ -0,0 +1,9 @@
+//const prompt = require('prompt-sync')();
+
+let a = parseInt(prompt("Digite o primeiro número: "));
+let b = parseInt(prompt("Digite o segundo número: "));
+
+console.log("A soma dos dois números é: ", + a+b);
+console.log("A subtração dos dois números é: ", + a-b);
+console.log("A multiplicação dos dois números é: ", + a*b);
+console.log("A divisão dos dois números é: ", + a/b);
---
+++ PW03 31-3-26/script.js
@@ -0,0 +1,7 @@
+var objpf = {
+    nome: "Thiago",
+    idade: 32,
+    profissao: "Programador",
+    estatrabalhando: false,
+};
+    console.log(objpf.estatrabalhando = true);
